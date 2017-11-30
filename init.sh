47c03ee17e5ee5132af2272e1027ab8b9d5806ff
Unknown <lexh94@hotmail.com>
2017-11-30 21:22:50 +0100

    CHA: Updated script

--- init.sh
+++ init.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 #Installation script for salt-minions. 
-#Designed for Ubuntu Xenial and Debian Stretch but should work for other distros too.
+#Designed for Ubuntu Xenial but should work for other distros too.
 apt update && apt -y upgrade
 echo $'Enter IP address of salt master:\n'
 read SaltIP
 #Edit hosts file.
 echo ''$SaltIP ' salt' >> /etc/hosts
 #Needed for debian to be able to download docker properly.
-sudo apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
-
 sudo apt-get -y install salt-minion
 sudo apt-get -y install python-pip python-dev build-essential 
 sudo pip install --upgrade pip 
@@ -24,4 +17,4 @@ echo $'Enter minion name:\n'
 read minionname
 echo "id: "$minionname >> /etc/salt/minion
 sudo systemctl restart salt-minion
-echo $'Done, remember to accept the key of this minion on your salt master.\n'
+echo 'Done, remember to accept the key of this minion on your salt master.\n'
